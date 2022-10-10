0df23f56166a5bf2f8711046d46312ff56b7bdce
Charles Werk <charles.werk@umconnect.umt.edu>
2022-10-09 22:14:44 -0600

    Added Recycler View and OnClickListener

--- app/src/main/java/com/example/spendless/MainActivity.kt
+++ app/src/main/java/com/example/spendless/MainActivity.kt
@@ -2,10 +2,67 @@ package com.example.spendless
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.spendless.model.UserData
+import com.example.spendless.view.UserAdapter
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var addsBtn: FloatingActionButton
+    private lateinit var recy: RecyclerView
+    private lateinit var userList: ArrayList<UserData>
+    private lateinit var userAdapter: UserAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+//  set list
+        userList = ArrayList()
+
+//        set find ID
+        addsBtn = findViewById(R.id.addingBtn)
+        recy = findViewById(R.id.budgetRecycler)
+//        set adapter
+        userAdapter = UserAdapter(this, userList)
+//        set Recycler view adapter
+        recy.layoutManager = LinearLayoutManager(this)
+        recy.adapter = userAdapter
+
+//        set Dialog
+        addsBtn.setOnClickListener {
+            addInfo()
+        }
     }
+
+    private fun addInfo() {
+        val inflater = LayoutInflater.from(this)
+        val v = inflater.inflate(R.layout.add_item, null)
+//        set view
+        val item = v.findViewById<EditText>(R.id.itemName)
+        val price = v.findViewById<EditText>(R.id.price)
+        val addDialog = AlertDialog.Builder(this)
+        addDialog.setView(v)
+        addDialog.setPositiveButton("Ok") { dialog, _ ->
+            val items = item.text.toString()
+            val itemPrice = price.text
+            userList.add(UserData("Item Name: $items", "Item Price: $$itemPrice"))
+            userAdapter.notifyDataSetChanged()
+            Toast.makeText(this, "Adding Item to List Success", Toast.LENGTH_SHORT).show()
+            dialog.dismiss()
+        }
+        addDialog.setNegativeButton("Cancel") { dialog, _ ->
+            dialog.dismiss()
+            Toast.makeText(this, "Cancel", Toast.LENGTH_SHORT).show()
+        }
+        addDialog.create()
+        addDialog.show()
+    }
+
+
+
+
 }
---
+++ app/src/main/java/com/example/spendless/model/UserData.kt
@@ -0,0 +1,3 @@
+package com.example.spendless.model
+
+data class UserData(val item:String, val price:String)
